d0c8d496818f904073ffcf95e81311d4f93e4365
rafiq <rafiqr169q@gmail.com>
2018-10-22 10:52:34 +0600

    All files added and login, logout functionality added

---
+++ classes/Adminlogin.php
@@ -0,0 +1,45 @@
+<?php
+include_once '../helpers/Format.php';
+include_once '../lib/Database.php';
+
+?>
+<?php
+class Adminlogin
+{
+    private $db;
+    private $fm;
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->fm = new Format();
+    }
+
+    public function adminLogin($adminUser, $adminPass)
+    {
+        $adminUser = $this->fm->validation($adminUser);
+        $adminPass = $this->fm->validation($adminPass);
+
+        $adminUser = mysqli_real_escape_string($this->db->link, $adminUser);
+        $adminPass = mysqli_real_escape_string($this->db->link, $adminPass);
+
+        if (empty($adminUser) || empty($adminPass)) {
+            $loginmsg = "Username or password must not be empty";
+            return $loginmsg;
+        } else {
+            $query = "SELECT * FROM tbl_admin WHERE adminUser = '$adminUser' AND adminPass = '$adminPass'";
+            $result = $this->db->select($query);
+            if ($result != false) {
+                $value = $result->fetch_assoc();
+                Session::set("adminlogin", true);
+                Session::set("adminId", $value['adminId']);
+                Session::set("adminUser", $value['adminUser']);
+                Session::set("adminName", $value['adminName']);
+                header("Location: index.php");
+            } else {
+                $loginmsg = "Username or password not match";
+                return $loginmsg;
+            }
+        }
+
+    }
+}
